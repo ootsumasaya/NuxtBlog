b20f8fd318059999289d76a6a205e9c8e39c49c8
sonokonogi <w215427c@yokohama-cu.ac.jp>
2021-12-27 16:34:39 +0900

    2 files added

---
+++ pages/uke.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-app>
+    <navi />
+    <v-main>
+    <h1>受け</h1>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import navi from '/components/navi';
+export default {
+  components: {
+    navi,
+  },
+};
+</script>
